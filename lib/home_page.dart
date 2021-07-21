9da5b09479ee44432eb773c6f7efd94e16986dd5
Tanvi129 <tanvigupta129@gmail.com>
2021-07-21 16:16:09 +0530

    First Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    var name = "Tanvi Gupta";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome To $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
