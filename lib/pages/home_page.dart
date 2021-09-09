061f6f23a48a93f95ab822d0db68b79ea77b8c29
Tanvi129 <tanvigupta129@gmail.com>
2021-09-10 02:37:42 +0530

    ListViewBuilder , ListItemWidget , Cards and assets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_first/models/catalog.dart';
 import 'package:flutter_app_first/widgets/drawer.dart';
+import 'package:flutter_app_first/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,13 +12,15 @@ class HomePage extends StatelessWidget {
     var name = "Tanvi Gupta";
     return Scaffold(
       appBar: AppBar(
-        
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome To $days days of flutter by $name"),
-        ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: CatalogModel.items[index]);
+            }),
       ),
       drawer: MyDrawer(),
     );
