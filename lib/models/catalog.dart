17e06b7bea89fbf42f0b576888750d5da8366720
Tanvi129 <tanvigupta129@gmail.com>
2021-10-06 22:32:05 +0530

    Json file, mapping to json file, constructors , loading data

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,20 +1,20 @@
 class CatalogModel {
-  static final items = [
-    Item(
-        id: 1,
-        name: "iPhone 12 Pro",
-        desc: "Apple iPhone 12th generation",
-        price: 999,
-        color: "#33505a",
-        image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"),
-    Item(
-      id: 2,
-      name: "Laptop",
-      price: 80000,
+  static List<Item> items = [
+    // Item(
+    //     id: 1,
+    //     name: "iPhone 12 Pro",
+    //     desc: "Apple iPhone 12th generation",
+    //     price: 999,
+    //     color: "#33505a",
+    //     image:
+    //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"),
+    // Item(
+    //   id: 2,
+    //   name: "Laptop",
+    //   price: 80000,
     
-      color: "#33505a",
-    )
+    //   color: "#33505a",
+    // )
   ];
 }
 
@@ -33,4 +33,25 @@ class Item {
       required this.price,
       this.color,
       this.image});
+
+      factory Item.fromMap(Map<String,dynamic> map){
+        
+        return Item(
+         id: map["id"],
+         name: map["name"], 
+         desc: map["desc"], 
+         price: map["price"],
+         color: map["color"],
+         image: map["image"]);
+      }
+
+      toMap()=>{
+        id: "id",
+         name: "name", 
+         desc: "desc", 
+         price: "price",
+         color: "color",
+         image: "image"
+
+      };
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app_first/models/catalog.dart';
 import 'package:flutter_app_first/widgets/drawer.dart';
 import 'package:flutter_app_first/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+   @override
+    void initState() {
+      super.initState();
+      loadData();
+    }
+
+    loadData() async {
+      //await Future.delayed(Duration(seconds: 2));
+      final catalogJson =
+          await rootBundle.loadString("assets/files/catalog.json");
+      final decodedData = jsonDecode(catalogJson);
+      var productData = decodedData["products"];
+      CatalogModel.items = List.from(productData)
+          .map<Item>((item) => Item.fromMap(item))
+          .toList();
+      setState(() {});
+    }
   @override
   Widget build(BuildContext context) {
     int days = 30;
     var name = "Tanvi Gupta";
-    final dummylist = List.generate(20, (index) => CatalogModel.items[0]);
+    //final dummylist = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
@@ -18,10 +44,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(16),
         child: ListView.builder(
-            itemCount: dummylist.length,
+            itemCount: CatalogModel.items.length,
             itemBuilder: (context, index) {
               return ItemWidget(
-                item: dummylist[index],
+                item: CatalogModel.items[index],
               );
             }),
       ),
